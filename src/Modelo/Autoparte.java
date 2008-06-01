adb43b7fbec74a97d3b91c12bdbc83801a08cadd
chuchu132 <chuchu132@646b7b16-a54c-0410-92b5-110a10c73e78>
2008-06-01 19:29:16 +0000

    :P jaja me olvide de  agregar los archivos "nuevos" al control de versiones

---
+++ src/Modelo/Autoparte.java
@@ -0,0 +1,60 @@
+package Modelo;
+
+import java.util.Observable;
+
+//import Excepciones.ProblemaTecnicoException;
+
+public abstract class Autoparte extends Observable {
+	private double peso;
+	private double precio;
+	private long vidaUtil;
+	private long vidaUtilInicial;
+	private long porcentajeVidaUtil;
+	private final double porcentajeVidaUtilMinima = 20; //en porcentaje
+	
+	public Autoparte(double peso, double precio, long vidaUtilInicial){
+		this.peso = peso;
+		this.precio = precio;
+		this.vidaUtil = this.vidaUtilInicial = vidaUtilInicial;
+		
+	}
+	
+	public double getPorcentajeVidaUtil() {
+		return (vidaUtil*100)/vidaUtilInicial;
+	}
+	
+	public double getVidaUtilMinima(){
+		return porcentajeVidaUtilMinima;
+	}
+	
+	public String toString(){
+		return ( " Precio: Algo$ " + precio + " , Vida Util: " + (vidaUtil * 100 ) + " % ");
+	}
+	
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public double getVidaUtil() {
+		return vidaUtil;
+	}
+	/**
+	 * Si se gasta el 10% pongo 0.10
+	 * */
+	public void gastar(double desgaste){
+		if(vidaUtil - desgaste >= 0)
+			vidaUtil -= desgaste;
+		else
+			vidaUtil = 0;
+	}
+	
+	public double getPeso(){
+		return this.peso;
+	}
+	
+	//abstract public void simular(double tiempo)throws ProblemaTecnicoException;
+	
+
+}
